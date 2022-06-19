6adba70b4af30c4b8a66e13e472baa558cd10459
Heung <80873447+sinheungheung@users.noreply.github.com>
2022-06-19 16:07:51 +0900

    더 많은 배수 구하기

---
+++ 더 많은 배수 구하기.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int func_a(int arr[], int arr_len) {
+    int count = 0;
+    for (int i = 0; i < arr_len; ++i)
+        if (arr[i] % 5 == 0)
+            count += 1;
+    return count;
+}
+char* func_b(int three, int five) {
+    if (three > five)
+        return "three";
+    else if (three < five)
+        return "five";
+    else
+        return "same";
+}
+int func_c(int arr[], int arr_len) {
+    int count = 0;
+    for (int i = 0; i < arr_len; ++i)
+        if (arr[i] % 3 == 0)
+            count += 1;
+    return count;
+}
+char* solution(int arr[], int arr_len) {
+    int count_three = func_c(arr, arr_len);
+    int count_five = func_a(arr, arr_len);
+    char* answer = func_b(count_three, count_five);
+    return answer;
+}
+int main() {
+    int arr[10] = { 2, 3, 6, 9, 12, 15, 10, 20, 22, 25 };
+    int arr_len = 10;
+    char* ret = solution(arr, arr_len);
+
+    printf("solution 함수의 반환 값은 %s 입니다.\n", ret);
+}
